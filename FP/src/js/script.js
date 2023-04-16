d59198ad4cda489ec6f10990693958e70c82f4fb
IvaKal <ivanekaloiani@gmail.com>
2023-04-16 23:51:09 +0400

    Contact and started slider

--- FP/src/js/script.js
+++ FP/src/js/script.js
@@ -6,16 +6,44 @@ function setupNav() {
   const dropdownWomen = navElement.querySelector(".dropdown__women");
   menLink.addEventListener("mouseenter", function () {
     dropdownMen.classList.add("open");
-  });
-  menLink.addEventListener("mouseleave", function () {
-    dropdownMen.classList.remove("open");
+    dropdownWomen.classList.remove("open");
   });
   womenLink.addEventListener("mouseenter", function () {
     dropdownWomen.classList.add("open");
+    dropdownMen.classList.remove("open");
   });
-  womenLink.addEventListener("mouseleave", function () {
+
+  navElement.addEventListener("mouseleave", function () {
+    dropdownMen.classList.remove("open");
     dropdownWomen.classList.remove("open");
   });
 }
 
 setupNav();
+
+function slider(data) {
+  const trendingSlider = document.querySelector(".trending");
+  if (trendingSlider == null) {
+    return;
+  }
+  const sliderElement = trendingSlider.querySelector(".slider");
+  const trendingSlice = data.slice(0, 5);
+  for (let i = 0; i < 5; i++) {
+    const product = trendingSlice[i];
+    const div = document.createElement("div");
+    div.innerHTML = '<img src="' + product.image + '">';
+    sliderElement.appendChild(div);
+    div.style.left= i*15+10+"%"
+    if(i==2){
+      div.classList.add("active")
+    }
+  }
+}
+
+async function fetchData() {
+  const response = await fetch("/FP/src/data/stock.json");
+  const data = await response.json();
+  slider(data);
+}
+
+fetchData();
